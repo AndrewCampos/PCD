f49265dceaefbbb85f279f4b42ee69ff532d7f65
AndrewCampos <a.campos98@live.com>
2021-01-30 23:16:44 -0300

    Jogo da vida nao funciona para 2048

--- 3 - MPI/Exemplos/send_sample.c
+++ 3 - MPI/Exemplos/send_sample.c
@@ -6,21 +6,19 @@
 int main(int argc, char *argv[]){
     int ierr,destino,origem,tag=0;
     int rank; // Id do processo
-    char bufRcv[5], **bufSnd;
+    int bufRcv[5][2], bufSnd[5][2] = {{0,1},{0,2},{0,3},{0,4},{0,5}};
 
-    bufSnd = malloc(5*sizeof(char));
-    bufSnd = {1,2,3,4,5};
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
     if(rank == 0){
         destino = rank+1;
-        ierr = MPI_Send(bufSnd,5,MPI_INT,destino,tag,MPI_COMM_WORLD);
-        printf("Processo %d enviou: %d\n""Send Status: %d\n",rank,bufSnd[0][0],ierr);
+        ierr = MPI_Send(bufSnd,5*2,MPI_INT,destino,tag,MPI_COMM_WORLD);
+        printf("Processo %d enviou: %d\n""Send Status: %d\n",rank,bufSnd[1][1],ierr);
     }else{
         origem = rank-1;
-        ierr = MPI_Recv(bufRcv,5,MPI_INT,origem,tag,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-        printf("Processo 0 recebeu: %d\n""Receive Status: %d\n",bufSnd[0][0],ierr);
+        ierr = MPI_Recv(bufRcv,5*2,MPI_INT,origem,tag,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        printf("Processo 0 recebeu: %d\n""Receive Status: %d\n",bufSnd[1][1],ierr);
     }
 
     MPI_Finalize();
